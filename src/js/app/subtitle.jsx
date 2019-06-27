da233943021ea48f1ac51d11f7d38bb27a173a0f
javm <quetzal1910@gmail.com>
2019-06-27 00:15:06 -0500

    Adding editable grid table

---
+++ src/js/app/subtitle.jsx
@@ -0,0 +1,45 @@
+import { h, render, Component } from 'preact';
+import ReactDOM from 'react-dom';
+import ReactDataGrid from 'react-data-grid';
+
+class Subtitle extends Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      rows: props.rows,
+      columns: props.columns
+    }
+  };
+
+  onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    this.setState(state => {
+      const rows = state.rows.slice();
+      for (let i = fromRow; i <= toRow; i++) {
+        rows[i] = { ...rows[i], ...updated };
+      }
+      return { rows };
+    });
+  };
+  render(props, state) {
+    return (
+      <ReactDataGrid
+        columns={this.state.columns}
+        rowGetter={i => this.state.rows[i]}
+        rowsCount={this.state.rows.length}
+        onGridRowsUpdated={this.onGridRowsUpdated}
+        enableCellSelect={true}
+        cellRangeSelection={{
+          onStart: args => console.log(this.state.rows),
+          onUpdate: args => console.log(this.state.rows),
+          onComplete: args => console.log(this.state.rows)
+        }}
+      />
+    );
+  }
+}
+
+export function showSubtitle(el, rows, columns) {
+  console.log(rows);
+    render(<Subtitle rows={rows} columns={columns}/>, el);
+}
